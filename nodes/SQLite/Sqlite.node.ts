1253fb1bf6852b5ae9362a763b3873c14d5ed5a7
Lucas <lucas.souza@edulabzz.com.br>
2022-10-24 16:25:54 -0300

    fix: update admin users

--- nodes/SQLite/Sqlite.node.ts
+++ nodes/SQLite/Sqlite.node.ts
@@ -44,6 +44,18 @@ export class Sqlite implements INodeType {
 				type: 'options',
 				options: [
 					{
+						name: 'CreateFile',
+						value: 'create',
+						description: 'Delete rows in database',
+						action: 'Delete rows in database',
+					},
+					{
+						name: 'Delete',
+						value: 'delete',
+						description: 'Delete rows in database',
+						action: 'Delete rows in database',
+					},
+					{
 						name: 'Execute Query',
 						value: 'executeQuery',
 						description: 'Execute an SQL query',
@@ -60,12 +72,6 @@ export class Sqlite implements INodeType {
 						value: 'update',
 						description: 'Update rows in database',
 						action: 'Update rows in database',
-					},
-					{
-						name: 'Delete',
-						value: 'delete',
-						description: 'Delete rows in database',
-						action: 'Delete rows in database',
 					},
 				],
 				default: 'insert',
@@ -184,83 +190,82 @@ export class Sqlite implements INodeType {
 		let returnItems: any[] = [];
 
 		try {
-			if (operation === 'executeQuery') {
-				// ----------------------------------
-				//         executeQuery
-				// ----------------------------------
+			switch (operation) {
+				case 'executeQuery':
+					const rawQuery = this.getNodeParameter('query', 0) as string;
+					const queryResult = await db.all(rawQuery);
 
-				const rawQuery = this.getNodeParameter('query', 0) as string;
-				const queryResult = await db.all(rawQuery);
-
-				returnItems = this.helpers.returnJsonArray(queryResult as unknown as IDataObject[]);
-			} else if (operation === 'insert') {
-				// ----------------------------------
-				//         insert
-				// ----------------------------------
-
-				try {
-					const table = this.getNodeParameter('table', 0) as string;
-					const columnString = this.getNodeParameter('columns', 0) as string;
-					const columns = columnString.split(',').map((column) => column.trim());
-					const insertItems = copyInputItems(items, columns);
-					const insertPlaceholder = `(${columns.map((column) => '?').join(',')})`;
-
-					const insertSQL = `INSERT INTO ${table}(${columnString}) VALUES ${items
-						.map((item) => insertPlaceholder)
-						.join(',')};`;
-					const queryItems = insertItems.reduce(
-						(collection: any, item: any) => collection.concat(Object.values(item as any)),
-						[],
-					); // tslint:disable-line:no-any
-					const queryResult = await db.run(insertSQL, queryItems);
-
-					returnItems = this.helpers.returnJsonArray(queryResult as unknown as IDataObject);
-				} catch (error) {
-					if (this.continueOnFail()) {
-						returnItems = this.helpers.returnJsonArray({ error: error.message });
-					} else {
-						await db.close();
-						throw error;
-					}
-				}
-			} else if (operation === 'update') {
-				// ----------------------------------
-				//         update
-				// ----------------------------------
-
-				try {
-					const table = this.getNodeParameter('table', 0) as string;
-					const updateKey = this.getNodeParameter('updateKey', 0) as string;
-					const columnString = this.getNodeParameter('columns', 0) as string;
-					const columns = columnString.split(',').map((column) => column.trim());
-
-					if (!columns.includes(updateKey)) {
-						columns.unshift(updateKey);
-					}
-
-					const updateItems = copyInputItems(items, columns);
-					const updateSQL = `UPDATE ${table} SET ${columns
-						.map((column) => `${column} = ?`)
-						.join(',')} WHERE ${updateKey} = ?;`;
-					const queryQueue = updateItems.map((item: any) =>
-						db.run(updateSQL, Object.values(item).concat(item[updateKey])),
-					);
-					const queryResult = await Promise.all(queryQueue);
 					returnItems = this.helpers.returnJsonArray(queryResult as unknown as IDataObject[]);
-				} catch (error) {
-					if (this.continueOnFail()) {
-						returnItems = this.helpers.returnJsonArray({ error: error.message });
-					} else {
-						await db.close();
-						throw error;
+					break;
+
+				case 'insert':
+					try {
+						const table = this.getNodeParameter('table', 0) as string;
+						const columnString = this.getNodeParameter('columns', 0) as string;
+						const columns = columnString.split(',').map((column) => column.trim());
+						const insertItems = copyInputItems(items, columns);
+						const insertPlaceholder = `(${columns.map((column) => '?').join(',')})`;
+
+						const insertSQL = `INSERT INTO ${table}(${columnString}) VALUES ${items
+							.map((item) => insertPlaceholder)
+							.join(',')};`;
+						const queryItems = insertItems.reduce(
+							(collection: any, item: any) => collection.concat(Object.values(item as any)),
+							[],
+						); // tslint:disable-line:no-any
+						const queryResult = await db.run(insertSQL, queryItems);
+
+						returnItems = this.helpers.returnJsonArray(queryResult as unknown as IDataObject);
+					} catch (error) {
+						if (this.continueOnFail()) {
+							returnItems = this.helpers.returnJsonArray({ error: error.message });
+						} else {
+							await db.close();
+							throw error;
+						}
 					}
-				}
-			} else {
-				await db.close();
-				throw new NodeOperationError(
-					this.getNode(),
-					`The operation "${operation}" is not supported!`,
-				);
+				case 'update':
+					try {
+						const table = this.getNodeParameter('table', 0) as string;
+						const updateKey = this.getNodeParameter('updateKey', 0) as string;
+						const columnString = this.getNodeParameter('columns', 0) as string;
+						const columns = columnString.split(',').map((column) => column.trim());
+
+						if (!columns.includes(updateKey)) {
+							columns.unshift(updateKey);
+						}
+
+						const updateItems = copyInputItems(items, columns);
+						const updateSQL = `UPDATE ${table} SET ${columns
+							.map((column) => `${column} = ?`)
+							.join(',')} WHERE ${updateKey} = ?;`;
+						const queryQueue = updateItems.map((item: any) =>
+							db.run(updateSQL, Object.values(item).concat(item[updateKey])),
+						);
+						const queryResult = await Promise.all(queryQueue);
+						returnItems = this.helpers.returnJsonArray(queryResult as unknown as IDataObject[]);
+					} catch (error) {
+						if (this.continueOnFail()) {
+							returnItems = this.helpers.returnJsonArray({ error: error.message });
+						} else {
+							await db.close();
+							throw error;
+						}
+					}
+					break;
+				case 'create':
+					new sqlite3.Database(filename, sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE, (err) => {
+						// do your thing
+						console.log('eero');
+					});
+
+				default:
+					await db.close();
+					throw new NodeOperationError(
+						this.getNode(),
+						`The operation "${operation}" is not supported!`,
+					);
+					break;
 			}
 		} catch (error) {
 			console.log(error);
